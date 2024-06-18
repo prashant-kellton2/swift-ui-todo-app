ad4e5efeea68ec95c4403ee4585307dc3a3958c2
prashant.maharana <prashant.maharana@kellton.com>
2024-06-18 13:37:55 +0530

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Prashant Maharana on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
